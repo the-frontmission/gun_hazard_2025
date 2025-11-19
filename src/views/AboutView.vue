57811cae3ebed12b5bc161e7351dd0041e5a6760
yphilkim <yphilkim@inter-elec.com>
2025-11-19 16:30:50 +0900

    add vue-router

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Welcome to Gun Hazard 2025</h1>
+    <p>This is the about view of the application.</p>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Gun Hazard 2025</h1>
+    <p>This is the home view of the application.</p>
+  </div>
+</template>
