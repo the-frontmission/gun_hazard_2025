8f8120455d15c66a174cddae06997cdef3f63f02
yphilkim <yphilkim@inter-elec.com>
2025-11-24 17:35:44 +0900

    fixed

--- src/components/app/AppNavbar.vue
+++ src/components/app/AppNavbar.vue
@@ -3,19 +3,21 @@ import  { routes } from '../../router/index';
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <nav class="navbar navbar-expand bg-dark" data-bs-theme="dark">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">GunHazard</router-link>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item" v-for="(route, index) in routes" :key="index">
-            <router-link
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <router-link class="navbar-brand" to="/">GunHazard</router-link>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav navbar-nav-scroll">
+            <li class="nav-item" v-for="(route, index) in routes" :key="index">
+              <router-link
               class="nav-link text-capitalize"
               active-class="active"
               :to="route.path"
-            >{{ route.name }}</router-link>
-          </li>
-        </ul>
+              >{{ route.name }}</router-link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
--- src/views/HoView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <div class="home">
-    <h1>Welcome to Gun Hazard 2025</h1>
-    <p>This is the home view of the application.</p>
-  </div>
-</template>
